4af2d8b6f809342320648fec40ef86e7b6c946cd
Erwan Mas <erwan@mas.nom.fr>
2023-02-05 12:24:57 -0500

    add 2 indexes to location_history

--- test/create_tab_location_history.sql
+++ test/create_tab_location_history.sql
@@ -4,5 +4,7 @@ CREATE TABLE `location_history` (
   `location_gps_x` float DEFAULT NULL,
   `location_gps_y` float DEFAULT NULL,
   `location_ip` varchar(64) DEFAULT NULL,
-  KEY `ix_clientid_location_time` (`clientid`,`location_time`)
+  KEY `ix_clientid_location_time` (`clientid`,`location_time`),
+  KEY `ix_location_gps_x_location_gps_y` (`location_gps_x`,`location_gps_y`),
+  KEY `ix_location_ip` (`location_ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
