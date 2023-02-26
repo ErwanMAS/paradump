a33acc9fd9fa1d0c4e9993b6d0afc0d3f8f7d72a
Erwan Mas <erwan@mas.nom.fr>
2023-02-26 18:30:04 -0500

    new dataset cosmetic changes remove a primary key , replace by on regular index

--- test/create_tab_ticket_history.sql
+++ test/create_tab_ticket_history.sql
@@ -1,8 +1,12 @@
 CREATE TABLE `ticket_history` (
   `ticketid` bigint(20) unsigned NOT NULL,
   `state` int(11) NOT NULL,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `topic` varchar(32) DEFAULT NULL,
   `human_factor` double DEFAULT NULL,
-  PRIMARY KEY (`ticketid`,`modified`,`state`)
+  `category` int(10) DEFAULT NULL,
+  `data` text NOT NULL,
+  KEY `ix_ticketid` (`ticketid`),
+  KEY `ix_modified` (`modified`),
+  KEY `ix_state` (`state`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
