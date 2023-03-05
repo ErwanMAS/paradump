ff30904c6e45973ce8f41f7fe9a08eaaf04b9f2a
Erwan Mas <erwan@mas.nom.fr>
2023-03-05 11:26:30 -0500

    reorder columns to have a string in the first column

--- test/create_tab_ticket_tag.sql
+++ test/create_tab_ticket_tag.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ticket_tag` (
-  `id` 	       bigint(20) unsigned NOT NULL,
   `lang_iso3`     varchar(6) ,
+  `id` 	       bigint(20) unsigned NOT NULL,
   `label`         varchar(128) ,
   `label_hex_u8`  varchar(256) ,
   `label_hex_l1`  varchar(256) ,  
